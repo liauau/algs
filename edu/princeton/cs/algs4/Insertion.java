433f56e9132762a5141a37c2cf078be3411f5406
liauau <liauau2009@qq.com>
2021-11-09 23:30:03 +0800

    Fix the insertion sort bug.

--- edu/princeton/cs/algs4/Insertion.java
+++ edu/princeton/cs/algs4/Insertion.java
@@ -58,29 +58,29 @@ public class Insertion {
      * @param a the array to be sorted
      */
     public static void sort(Comparable[] a) {
-        boolean no_optimize = false;
+        boolean is_optimize = false;
 
-        if (no_optimize) {
+        if (is_optimize) {
             int n = a.length;
-            for (int i = 1; i < n; i++) {
-                for (int j = i; j > 0 && less(a[j], a[j-1]); j--) {
-                    exch(a, j, j-1);
+            for (int i = 0; i < n; i++) {
+                int j = i;
+                Comparable tmp = a[j];
+                while ((j > 0) && less(tmp, a[j - 1])) {
+                    a[j] = a[j - 1];
+                    j--;
                 }
-                //assert isSorted(a, 0, i);
+                a[j] = tmp;
             }
             assert isSorted(a);
         }
         else
         {
             int n = a.length;
-            for (int i = 0; i < n; i++) {
-                int j = i;
-                Comparable tmp = a[j];
-                while ((j > 0) && less(a[j], a[j - 1])) {
-                    a[j] = a[j - 1];
-                    j--;
+            for (int i = 1; i < n; i++) {
+                for (int j = i; j > 0 && less(a[j], a[j-1]); j--) {
+                    exch(a, j, j-1);
                 }
-                a[j] = tmp;
+                assert isSorted(a, 0, i);
             }
             assert isSorted(a);
         }
